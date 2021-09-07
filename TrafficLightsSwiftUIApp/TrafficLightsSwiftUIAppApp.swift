0a33c3d611c6fc35d69a42c23a95097a14787ad1
Sergey <rumyantsev.serge@yandex.ru>
2021-09-07 12:52:47 +0300

    Initial Commit

---
+++ TrafficLightsSwiftUIApp/TrafficLightsSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSwiftUIAppApp.swift
+//  TrafficLightsSwiftUIApp
+//
+//  Created by Sergey on 07.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
